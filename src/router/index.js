50a26e85a2c9a445fb69448e672e4b0d1ab53c3c
贾占霞 <jiazhanxia@beisen.com>
2017-06-27 20:59:12 +0800

    add css

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Hello',
+//       component: Hello
+//     }
+//   ]
+// })
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: Home
     }
   ]
 })
